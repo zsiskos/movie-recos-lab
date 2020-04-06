745ad07745228e126584805c709caf141aff6dac
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-04-06 14:21:15 -0400

    set MVT for about, reco, new

--- recocollector/main_app/urls.py
+++ recocollector/main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('new/', views.new_reco, name='new_reco'),
+    path('recommendations/', views.my_recos, name='my_recos'),
 ]
--- recocollector/main_app/views.py
+++ recocollector/main_app/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(req):
     return HttpResponse('<h1>this works</h1>')
+
+def about(req):
+    return HttpResponse('<h1>About page</>')
+
+def new_reco(req):
+    return HttpResponse('<h1>Add a New Recommendation</h1>')
+
+def my_recos(req):
+    return HttpResponse('<h1>My Recommendations</h1>')
