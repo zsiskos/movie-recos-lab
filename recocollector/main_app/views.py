449dbbb3c3828bf32357b52528acbfa4e9003038
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-04-07 08:29:53 -0400

    test model with images

--- recocollector/main_app/views.py
+++ recocollector/main_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Reco:
+    def __init__(self, title, how_heard, where, description):
+        self.title = title
+        self.how_heard = how_heard
+        self.where = where
+        self.description = description
+
+recos = [
+    Reco('Fight Club', 'Zoe', 'NetFlix', 'A mind fuck movie'),
+    Reco('Army of Darkness', 'James', 'NetFlix', 'The best B rated horror flick ever'),
+    Reco('Onward', 'John', 'Disney', 'a funny feel good Disney flick'),
+    Reco('The Outsider', 'James\'s work friend', 'Crave', 'same director as ozarks, basedo n stephen king, makes y ou think and creepy'),
+    Reco('The Servant', 'James', 'AppleTV', 'so creepy, dir. by M. Night Shamalaman'),
+    Reco('Bombshell', 'Ads', 'GooglePlay', 'true story, strong female characters'),
+    Reco('House of 1000 Corpses', 'Joe', 'Netflix', 'scary AF'),
+]
+
+
+
+
 # Create your views here.
 def home(req):
     return render(req, 'index.html')
@@ -12,4 +32,4 @@ def new_reco(req):
     return render(req, 'movies/new.html')
 
 def my_recos(req):
-    return render(req, 'movies/index.html')
+    return render(req, 'movies/index.html', {'recos': recos})
